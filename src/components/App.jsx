60e6163e7cc63f931a40473805bc620052879ff7
Ishan Sarma <ishan.baban@gmail.com>
2020-08-23 16:58:40 +0530

    render conditionally

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import Login from "./Login";
-var isLoggedIn = true;
-
+var isLoggedIn = false;
+function renderConditionally() {
+  if (isLoggedIn === true) {
+    return <h1>Hello</h1>;
+  } else {
+    return <Login />;
+  }
+}
 function App() {
-  return (
-    <div className="container">
-      <h1>Hello</h1>
-      <Login />
-    </div>
-  );
+  return <div className="container">{renderConditionally()}</div>;
 }
 
 export default App;
